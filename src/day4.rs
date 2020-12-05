5718d50ce6b002e03338abd782519f718003d085
Eric Holk <eric.holk@gmail.com>
2020-12-04 22:25:59 -0800

    Day 4, part 2

--- src/day4.rs
+++ src/day4.rs
@@ -2,13 +2,14 @@ use regex::Regex;
 use std::collections::HashMap;
 
 pub fn run<IO: std::io::BufRead>(input: IO) -> std::io::Result<()> {
-    println!("Part 1: {}", count_valid_passports(input)?);
+    println!("Part 2: {}", count_valid_passports(input)?);
     Ok(())
 }
 
 fn count_valid_passports<IO: std::io::BufRead>(input: IO) -> std::io::Result<usize> {
     let mut passport = String::new();
     let mut count = 0;
+
     for line in input.lines() {
         let line = line?;
         if line.trim() == "" {
@@ -38,12 +39,13 @@ fn parse_and_validate_passport(passport: &str) -> bool {
 
     let mut required_fields: HashMap<String, bool> =
         ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"]
-            .into_iter()
+            .iter()
             .map(|k| (k.to_string(), false))
             .collect();
 
     for m in RE.captures_iter(passport) {
         let key = &m["key"];
+        let value = &m["value"];
 
         if key == "cid" {
             continue;
@@ -54,9 +56,65 @@ fn parse_and_validate_passport(passport: &str) -> bool {
             Some(true) => panic!("no duplicate fields allowed"),
             Some(false) => (), // okay
         }
+
+        if !match key {
+            "byr" => 
+                validate_year(value, 1920, 2002),
+            "iyr" => validate_year(value, 2010, 2020),
+            "eyr" => validate_year(value, 2020, 2030),
+            "hgt" => validate_height(value),
+            "hcl" => validate_hair_color(value),
+            "ecl" => value == "amb" || value == "blu" || value == "brn" || value == "gry" || value == "grn" || value == "hzl" || value == "oth",
+            "pid" => validate_pid(value),
+            _ => false
+        } {
+            return false
+        }
     }
 
     required_fields.into_iter().all(|(_, v)| v)
+}
+
+fn validate_pid(hc: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^\d{9}$").unwrap();
+    }
+
+    RE.is_match(hc)
+}
+
+fn validate_hair_color(hc: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+    }
+
+    RE.is_match(hc)
+}
+
+fn validate_year(y: &str, min: usize, max: usize) -> bool {
+    if y.len() != 4 { return false; }
+    match y.parse::<usize>() {
+        Ok(y) => min <= y && y <= max,
+        Err(_) => false
+    }
+}
+
+fn validate_height(h: &str) -> bool {
+    match h.strip_suffix("cm") {
+        Some(h) => {
+            match h.parse::<usize>() {
+            Ok(h) => 150 <= h && h <= 193,
+        Err(_) => false}
+        },
+        None => match h.strip_suffix("in") {
+            Some(h) => {
+                match h.parse::<usize>() {
+                Ok(h) => 59 <= h && h <= 76,
+            Err(_) => false}
+            },
+            None => false
+        }
+    }
 }
 
 #[cfg(test)]
