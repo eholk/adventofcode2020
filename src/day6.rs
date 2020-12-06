334a2943c38ba1152e9dca53b2edece456aa75f4
Eric Holk <eric.holk@gmail.com>
2020-12-05 21:57:21 -0800

    Day 6, part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,11 @@
+use std::collections::HashSet;
+
 pub fn run<IO: std::io::Read>(mut input: IO) -> std::io::Result<()> {
     let mut data = String::new();
     input.read_to_string(&mut data)?;
 
     println!("Part 1: {}", count_groups(data.as_str()));
+    println!("Part 2: {}", count_groups_all(data.as_str()));
 
     Ok(())
 }
@@ -17,6 +20,16 @@ fn count_groups(input: &str) -> usize {
             }
         }
         count += answers.len()
+    }
+    count
+}
+
+fn count_groups_all(input: &str) -> usize {
+    let mut count = 0;
+    let all_answers = "abcdefghijklmnopqrstuvwxyz".chars().collect::<HashSet<char>>();
+    for group in input.split("\r\n\r\n") {
+        count += group.lines().map(|person| person.chars().collect::<HashSet<char>>())
+        .fold(all_answers.clone(), |a, b| a.intersection(&b).map(|&a| a).collect()).len();
     }
     count
 }
