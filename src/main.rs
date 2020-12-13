55040a19dd3a67d7d6a969f0f3df45e937076498
Eric Holk <eric.holk@gmail.com>
2020-12-13 08:32:12 -0800

    Day 12, part 1

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate regex;
 mod day1;
 mod day10;
 mod day11;
+mod day12;
 mod day2;
 mod day3;
 mod day4;
@@ -60,6 +61,9 @@ fn main() {
         }
         "day11" => {
             day11::run(input).unwrap();
+        }
+        "day12" => {
+            day12::run(input).unwrap();
         }
         day => {
             println!("Unknown day: {}", day);
