0bf7433c6eec74b01c16320eade60f8fc7f92dbe
Eric Holk <eric.holk@gmail.com>
2020-12-15 23:14:04 -0800

    Day 15, part 1 and 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,26 +1,59 @@
 use std::collections::HashMap;
+use std::iter::FromIterator;
 
 const INPUT: &[usize] = &[6, 13, 1, 15, 2, 0];
 
 pub fn run() {
     let last_spoken = play_game(INPUT, 2020);
     println!("Part 1: {}", last_spoken);
+
+    println!("Part 2: {}", play_game(INPUT, 30000000));
+}
+
+struct Game {
+    // Maps a number to the turn it was last spoken on, or None if it is new.
+    last_spoken_on_turn: HashMap<usize, usize>,
+    // the current turn
+    turn: usize,
+    // the number that will be spoken on this turn
+    spoken: usize,
+}
+
+impl Game {
+    fn new(init: &[usize]) -> Game {
+        Game {
+            turn: init.len(),
+            last_spoken_on_turn: HashMap::from_iter(
+                init.iter().enumerate().map(|(i, number)| (*number, i + 1)),
+            ),
+            spoken: init[init.len() - 1],
+        }
+    }
+
+    fn take_turn(&mut self) {
+        // it is currently turn self.turn, and we are going to say self.spoken
+        //
+        // We need to compute what we will say in turn+1.
+
+        match self.last_spoken_on_turn.insert(self.spoken, self.turn) {
+            None => self.spoken = 0,
+            Some(turn) => self.spoken = self.turn - turn,
+        }
+
+        self.turn += 1;
+    }
 }
 
 fn play_game(input: &[usize], turns: usize) -> usize {
-    let mut last_turns = HashMap::new();
-    let mut last_spoken = 0;
-    for turn in 1..turns {
-        last_spoken = if turn - 1 < input.len() {
-            input[turn - 1]
-        } else {
-            match last_turns.insert(last_spoken, turn - 1) {
-                Some(last_turn) => turn - last_turn,
-                None => 0,
-            }
+    if turns - 1 < input.len() {
+        input[turns - 1]
+    } else {
+        let mut game = Game::new(input);
+        while game.turn < turns {
+            game.take_turn();
         }
+        game.spoken
     }
-    last_spoken
 }
 
 #[cfg(test)]
